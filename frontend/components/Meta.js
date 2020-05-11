9d6798cb9c49ae81ccbe2cccb3b4ded4667f5507
Michael Dunlap <michaeldunlap@earthlink.net>
2020-05-10 23:48:32 -0700

    add favicon and nprogress from nextjs

---
+++ frontend/components/Meta.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+
+const Meta = () =>
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    <link rel="shortcut icon" type="image/x-icon" href="/static/favicon.ico" />
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+    <title>garb-n-go</title>
+  </Head>
+
+export default Meta
