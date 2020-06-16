2a9e969e8df7fcc9501f439b898dcda1ac628552
Michael Dunlap <michaeldunlap@earthlink.net>
2020-06-16 15:02:46 -0700

    trying a wider Access-Control-Allow-Origin to get around CORS problem

--- backend/src/index.js
+++ backend/src/index.js
@@ -9,6 +9,11 @@ const server = createServer()
 
 server.express.use(cookieParser())
 
+server.express.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
 // decode the JWT so we can get user id
 server.express.use((req, res, next) => {
   const { token } = req.cookies
